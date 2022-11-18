83744b8b9efedca9c517e9514db75de435cc8bcb
RaduPetruta <99913485+raduPetruta@users.noreply.github.com>
2022-11-18 16:09:43 +0200

    Add project files.

---
+++ Homework1_OopLab/Homework1_OopLab/a.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#pragma once
+#ifndef A_H
+#define A_H
+void test() {
+	cout << "Test function from a.h" << "\n";
+}
+#endif // !A_h
---
+++ Homework1_OopLab/Homework1_OopLab/b.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#pragma once
+#ifndef B_H
+#define B_H
+#include "a.h"
+void printText() {
+	cout << "Printing text from b.h" << "\n";
+}
+#endif // !B_H
